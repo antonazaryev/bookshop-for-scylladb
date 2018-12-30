941dd3d1780240312d31c624f41e00dfed6f056f
Anton Azaryev <antonazaryev@gmail.com>
2018-12-30 16:11:46 +0200

    first commit

---
+++ frontend/src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../styles/_header.scss';
+
+const Header =  ({ itemsInCart, openCartHandler }) => (
+  <div className="header">
+    <div className="title">
+      ScyllaDB Bookshop
+    </div>
+    <div className="my-cart" onClick={openCartHandler}>
+      My Cart ({ itemsInCart })
+    </div>
+  </div>
+);
+
+export default Header;
